c4c0553e2ca628bafbf03c04e967085bb5d80494
Daijoubu22 <alextrio.2224@gmail.com>
2022-11-27 22:02:23 +0300

    Add logo link

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ReactComponent as LogoIcon } from 'assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 import styles from './Logo.module.scss';
 
 function Logo() {
   return (
-    <span className={styles.main}>
-      <LogoIcon width="20" />
-      mangoread
-    </span>
+    <Link to="/">
+      <span className={styles.main}>
+        <LogoIcon width="20" />
+        mangoread
+      </span>
+    </Link>
   );
 }
 

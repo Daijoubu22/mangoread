4465c3d14588a9b94778a3f898b8b9f2f4faec7a
Daijoubu22 <alextrio.2224@gmail.com>
2022-12-12 20:05:19 +0300

    Cosmetic improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/_global.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 import App from './App';
+import './scss/_global.scss';
+import './scss/_preloader.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,

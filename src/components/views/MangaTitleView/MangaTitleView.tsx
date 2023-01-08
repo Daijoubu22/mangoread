c32c931cbb207db0b8ee6f84262f8a06b76ac436
Daijoubu22 <alextrio.2224@gmail.com>
2023-01-08 22:23:03 +0300

    Add link to title

--- src/components/views/MangaTitleView/MangaTitleView.tsx
+++ src/components/views/MangaTitleView/MangaTitleView.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Manga from 'services/models/Manga';
 import { getMangaAuthor } from 'services/utils/utils';
+import { Link } from 'react-router-dom';
 import styles from './MangaTitleView.module.scss';
 
-interface MangaTitileViewProps {
+interface MangaTitleViewProps {
   manga: Manga,
 }
 
-function MangaTitleView({ manga }: MangaTitileViewProps) {
+function MangaTitleView({ manga }: MangaTitleViewProps) {
   const author = getMangaAuthor(manga);
   const { title, year } = manga.attributes;
   return (
     <>
-      <h2 className={styles.title}>{title.en}</h2>
+      <Link to={`/manga/${manga.id}`}>
+        <h2 className={styles.title}>{title.en}</h2>
+      </Link>
       <p className={styles.author}>
         {author?.attributes && `${author.attributes.name}`}
         {author?.attributes && year && ' / '}
